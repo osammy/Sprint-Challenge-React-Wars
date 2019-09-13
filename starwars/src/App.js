8a306812a2d17e7cd1ebbe5b0671094556b1fc66
samuel <osamaimafidon@gmail.com>
2019-09-13 11:01:14 +0100

    refactor code

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,14 +1,14 @@
-import React, {useState,useEffect} from 'react';
-import Persons from './components/Persons/Persons';
-import axios from 'axios';
-import PaginationButtons from './components/PaginationButtons/PaginationButtons'
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Persons from "./components/Persons/Persons";
+import axios from "axios";
+import PaginationButtons from "./components/PaginationButtons/PaginationButtons";
+import "./App.css";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
@@ -19,48 +19,59 @@ const App = () => {
   const noOfPeoplePerPage = 10;
   let pagesCount;
 
-    useEffect(()=>{
-      axios.get(`https://swapi.co/api/people/?page=${pageNumber}`)
-      .then(response =>{
-        const people = response.data;
-        console.log(people);
+  useEffect(
+    () => {
+      axios
+        .get(`https://swapi.co/api/people/?page=${pageNumber}`)
+        .then(response => {
+          const people = response.data;
+          console.log(people);
 
-        setPeople(people.results);
-        if(pageNumber > 1) setdisablePrev(false);
-        //  if(pageNumber > 9) setdisableNext(true)
-        console.log(people.results.length)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  },[pageNumber])
+          setPeople(people.results);
+          if (pageNumber > 1) setdisablePrev(false);
+          //  if(pageNumber > 9) setdisableNext(true)
+          console.log(people.results.length);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    [pageNumber]
+  );
 
   function next() {
-    if(pageNumber >= Math.ceil(pagesCount / noOfPeoplePerPage) ){ 
-      alert("returning max page length")
+    if (pageNumber >= Math.ceil(pagesCount / noOfPeoplePerPage)) {
+      alert("returning max page length");
       return;
-    };
+    }
 
     setPageNumber(pageNumber + 1);
   }
 
   function prev() {
-    if(pageNumber <= 1) {
-       alert("invalid page number")
-      return
-    };
+    if (pageNumber <= 1) {
+      alert("invalid page number");
+      return;
+    }
     setPageNumber(pageNumber - 1);
   }
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <div class="container">
-      <Persons people={people}/>
-      <PaginationButtons disableNext={disableNext} disablePrev={disablePrev} next={next} prev={prev} noOfContentsDisplayed={people.length} page={pageNumber} />
+      <div className="container">
+        <Persons people={people} />
       </div>
-    </div>
+        <PaginationButtons
+          disableNext={disableNext}
+          disablePrev={disablePrev}
+          next={next}
+          prev={prev}
+          noOfContentsDisplayed={people.length}
+          page={pageNumber}
+        />
+      </div>
   );
-}
+};
 
 export default App;
